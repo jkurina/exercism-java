c35dedb96f743b3a639a3619a961d4d61eacfa46
Joseph Kurina <joe.kurina@gmail.com>
2017-10-24 23:43:00 +0000

    re-did the two-fer exercise again.

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,10 +1,9 @@
 //Rev. 2: got rid of the try/catch format to simplify the program.
+//Rev. 3: used a ternary operator to further simplify the program.
 import java.io.*;
 public class Twofer {
-    public String twofer(String name) {
-        if (name == null) {
-            name = "you";
-        }
+    public String twofer(String input) {
+        String name = input == null ? "you" : input;
         return "One for " + name + ", one for me.";
     }
 }
