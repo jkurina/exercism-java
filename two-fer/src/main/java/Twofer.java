6c49c7dac3f977007fef3f8933580fd0de8427fb
Joseph Kurina <joe.kurina@gmail.com>
2017-10-24 23:36:37 +0000

    rewrote the two-fer exercise

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,14 +1,10 @@
+//Rev. 2: got rid of the try/catch format to simplify the program.
 import java.io.*;
-
 public class Twofer {
     public String twofer(String name) {
-        try {
-            if (name.length() < 0) {
-                name = "you";
-            }
-            return "One for " + name + ", one for me.";
-        } catch (NullPointerException e) {
-            return "One for you, one for me.";
+        if (name == null) {
+            name = "you";
         }
+        return "One for " + name + ", one for me.";
     }
 }
