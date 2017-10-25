37b4f53f3d868b03f2a6f9b8e4f599e97847f8bc
Joseph Kurina <joe.kurina@gmail.com>
2017-10-25 15:34:38 +0000

    final simplification of the two-fer program

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,9 +1,8 @@
 //Rev. 2: got rid of the try/catch format to simplify the program.
 //Rev. 3: used a ternary operator to further simplify the program.
-import java.io.*;
+//Rev. 4: took out the import statement and simplified the method to one line.
 public class Twofer {
     public String twofer(String input) {
-        String name = input == null ? "you" : input;
-        return "One for " + name + ", one for me.";
+        return String.format( "One for %s, one for me.", input != null ? input : "you" );
     }
 }
